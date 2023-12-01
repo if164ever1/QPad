fbaf390b30baad8d6a20b1be40bfcfc512da6cb5
if164ever1 <acere5ihor@gmail.com>
2023-12-01 18:47:49 +0100

    create class Note

---
+++ QPadBackend/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace QPadBackend.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
