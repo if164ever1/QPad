c319e2fc54553828a155511bf3c20ae4c24fa4e1
if164ever1 <acere5ihor@gmail.com>
2023-12-01 18:52:33 +0100

    create class User

---
+++ QPadBackend/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QPadBackend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
